7dc438d00cc8384d1b38a21d13e91aa699d21363
Kasper <kasperkh.kh@gmail.com>
2024-09-20 18:55:24 +0200

    x

--- vite-plugin-electron/test/fixtures/external-foo.ts
+++
@@ -1,3 +0,0 @@
-export const message = 'foo'
-
-console.log(import('vite')) // in node_modules
--- vite-plugin-electron/test/fixtures/external-main.ts
+++
@@ -1,5 +0,0 @@
-import vite from 'vite' // in node_modules
-import { message } from './external-foo'
-
-console.log(vite)
-console.log(message)
